59596f1fe9bdb410544635650574cc291a726338
rejan.bajracharya <rejan.bajracharya@cotiviti.com>
2023-09-26 10:40:20 +0545

    fix: range

--- assignments/lambda.py
+++ assignments/lambda.py
@@ -3,12 +3,14 @@ from functools import reduce
 user_input = input("Enter the start & end for a range with delimiter ','");
 start,end = list(user_input.split(','))
 
-num_list = list(range(int(start), int(end)))
+num_list = list(range(int(start), int(end) + 1))
 print(num_list)
 
 even_sum = reduce(lambda x, y: x + y, list(filter(lambda item: item % 2 == 0, num_list)))
 odd_sum = reduce(lambda x, y: x + y, list(filter(lambda item: item % 2 != 0, num_list)))
 
-print(f"Odd even sum in the range {start},{end}: {even_sum + odd_sum}")
+print(f"Even sum in a range: {even_sum}")
+print(f"Odd sum in a range: {odd_sum}")
+print(f"Total sum in a range {start},{end}: {even_sum + odd_sum}")
 
 

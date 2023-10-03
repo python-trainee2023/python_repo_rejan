08a24eaabe47406d31f90c5fd589dd2db1f00159
rejan.bajracharya <rejan.bajracharya@cotiviti.com>
2023-10-03 09:19:26 +0545

    fix error

--- seleniumBasics/main.py
+++ seleniumBasics/main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 class BaseClass:
     def __init__(self):
         self.driver = None
@@ -13,31 +12,37 @@ class BaseClass:
     def teardown(self):
         self.driver.close()
 
-
 class SauceDemo(BaseClass):
     def __init__(self):
         super().__init__()
-        super().setup()
-        self.username = By.ID('user-name')
-        self.password = By.ID('password')
-        self.loginBtn = By.ID("login-button")
+        self.username = (By.ID, 'user-name')
+        self.password = (By.ID, 'password')
+        self.loginBtn = (By.ID, "login-button")
 
     def login(self, username, password):
         self.clearInput(self.username)
         self.inputText(self.username, username)
         self.clearInput(self.password)
         self.inputText(self.password, password)
-        self.driver.find_element(By.ID(self.loginBtn)).click()
+        self.driver.find_element(*self.loginBtn).click()  # Unpack the tuple with *
 
     def clearInputs(self):
         self.clearInput(self.username)
         self.clearInput(self.password)
 
     def clearInput(self, locator):
-        self.driver.find_element(locator).clear()
+        self.driver.find_element(*locator).clear()
 
     def inputText(self, locator, text):
-        self.driver.find_element(locator).send_keys(text)
+        self.driver.find_element(*locator).send_keys(text)
 
-    loginDemo = SauceDemo()
+# Instantiate the SauceDemo class
+loginDemo = SauceDemo()
+
+try:
+    loginDemo.setup()
     loginDemo.login("saucedemo", "secretsauce")
+except Exception as e:
+    print(e)
+finally:
+    loginDemo.teardown()
